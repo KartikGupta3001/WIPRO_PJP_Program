1242de87bdff895c90ff03e1faad285c71b3ae26
KartikGupta3001 <kartik.gupta2017@vitstudent.ac.in>
2021-05-30 17:51:39 +0530

    Delete TM08/TM08_Project/sales/SQLFiles directory

--- TM08/TM08_Project/sales/SQLFiles/VSalesReport.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW V_SALES_REPORT AS
-	SELECT Sales_ID, Sales_Date, Product_ID, Product_Name, 
-			Quantity_Sold, Product_Unit_Price, Sales_Price_Per_Unit, 
-			(Sales_Price_Per_Unit - Product_Unit_Price) Profit_Amount
-	FROM TBL_STOCK NATURAL JOIN TBL_SALES
-	ORDER BY Profit_Amount DESC, Sales_ID ASC;
